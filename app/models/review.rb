1d6d3a53e006442dd0dac1017a1241243f4db867
Pierre Bergue <pierre.bergue@gmail.com>
2020-11-11 15:36:45 +0100

    implemented the restaurant model create and read

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant, foreign_key: "restaurant_id"
+
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: 0..5 }
+  validates :rating, numericality: { only_integer: true }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# t.string "name"
+# t.string "address"
+# t.string "phone_number"
+# t.string "category"
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Name.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: categories.sample
+  )
+  restaurant.save!
+end
